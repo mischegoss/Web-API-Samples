61a0a0584095509331aa933704636985000a035a
Tamar <mischegoss@gmail.com>
2020-06-20 03:04:50 -0400

    Add HTML to timer

--- sample_timer.js
+++ sample_timer.js
@@ -3,6 +3,7 @@ function myTimer() {  // This my myTimer function wraps around everything else s
 //Set variables to use. 
 var startTime = 5;
 var myInterval = 3000;  //Length of time before it will resstart
+var timer = document.getElementById("timer");
 
 //Call the setInterval and give it a name 
 
@@ -10,7 +11,7 @@ var myInterval = 3000;  //Length of time before it will resstart
 var mySet = setInterval( createTimer, myInterval);
 
 //This is the function telling the what we want to do at each interval
-// You can amke it a nameless function (like examples in class) OR
+// You can make it a nameless function (like examples in class) OR
 //give it a name and call it by its  name. I think it is neater to 
 //give the function its own name like above.
 
@@ -21,13 +22,13 @@ function createTimer () {
 //You ONLY want the interval to keep going  IF your startTime is above zero
 
 if (startTime > 0) {
-console.log( startTime);
+timer.textContent = startTime;
 
 //This subtracts 1 from the start time
 startTime = startTime - 1
 
 } else {
-console.log("Done")
+timer.textContent = "DONE"
 
 // This is your clearInterval. The value it is going to take is the name you gave your 
 // setInterval (aee above). This code says, "Stop doing what mySet told you do do now"
